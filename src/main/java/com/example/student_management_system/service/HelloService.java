9091693660c2a1fcd8ba6784cb9cc81480233236
Esha Saha <sahaesha66@gmail.com>
2026-02-23 08:38:53 +0600

    Update hello message for PR

---
+++ src/main/java/com/example/student_management_system/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.example.student_management_system.service;
+
+public class HelloService {
+    // HelloService.java
+    public String getMessage() {
+        return "Hello Software Lab! [Updated]"; // add [Updated] or any small text
+    }
+}
